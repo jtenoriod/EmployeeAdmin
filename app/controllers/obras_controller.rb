aebe45ae72f5735c14c0105a934cd4068f007006
lucas nico zamora jabre <lzamora@alumnos.inf.utfsm.cl>
2017-01-31 11:53:33 -0300

    CRUD LISTO

--- app/controllers/obras_controller.rb
+++ app/controllers/obras_controller.rb
@@ -1,20 +1,45 @@
 class ObrasController < ApplicationController
-  def new
-    @obra = Obra.new
+
+  def index
+    @obra = Obra.all
   end
 
-  def create
-    @obra = Obra.new(obra_params)
-    @obra.save
-    redirect_to @obra
+  def new
+    @obra = Obra.new
   end
 
   def show
     @obra = Obra.find(params[:id])
   end
 
-  def index
-    @obra = Obra.all
+  def edit
+    @obra = Obra.find(params[:id])
+  end
+
+  def create
+    @obra = Obra.new(obra_params)
+    if @obra.save
+      redirect_to @obra
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @obra = Obra.find(params[:id])
+
+    if @obra.update(obra_params)
+      redirect_to @obra
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @obra = Obra.find(params[:id])
+    @obra.destroy
+
+    redirect_to obras_path
   end
 
   private
